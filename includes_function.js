147e19a5ab4f41fdf8c9ae342a0a57d266c001b2
jose marte <tumusicalatina@gmail.com>
2019-04-02 08:35:05 -0400

    added an includes function to search a string

---
+++ includes_function.js
@@ -0,0 +1,11 @@
+/*
+One cool way to see if a character is contained in a string is the includes() function
+*/ 
+
+var myString = "This is my string";
+
+if(myString.includes("my")){
+    console.log("yep");
+}else{
+    console.log("nope");
+}
