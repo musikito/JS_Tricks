1dc2830f7916d615bc924a3c4ec7c2ff3b49848f
JoséMiguel Marte <tumusicalatina@gmail.com>
2019-04-06 07:12:18 -0400

    Create IIFE.js

---
+++ IIFE.js
@@ -0,0 +1,9 @@
+/*
+Self-Invoked Anonymous Function or Immediately Invoked Function Expression (IIFE). 
+It is a function that executes automatically when you create it
+
+*/
+(function(a,b){
+    var result = a+b;
+    return result;
+})(10,20)
